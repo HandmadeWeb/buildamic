2795174f2f66543cc765342dc31f64926a1a28d5
Michael Rook <michael@handmadeweb.com.au>
2021-06-16 19:40:53 +1000

    Add more descriptive information.

--- resources/views/blade/default-field.blade.php
+++ resources/views/blade/default-field.blade.php
@@ -1,5 +1,8 @@
 @extends('buildamic::blade.layouts.field')
 
 @section('field_content')
-    <!-- Field {{ $field->get('uuid') }} could not be rendered -->
+    <!-- Field could not be rendered, View not found -->
+    <!-- ID: {{ $field->get('uuid') }} -->
+    <!-- Type: {{ $field->get('config')->get('type') }} -->
+    <!-- Handle: {{ $field->get('config')->get('handle') }} -->
 @overwrite
