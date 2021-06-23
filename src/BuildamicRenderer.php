e4367f1a3fc4780bf598235df784250ad73bf59c
Michael Rook <michael@handmadeweb.com.au>
2021-06-23 19:52:40 +1000

    Use Statamic View (Needs composer update)

--- src/BuildamicRenderer.php
+++ src/BuildamicRenderer.php
@@ -5,9 +5,8 @@ namespace Michaelr0\Buildamic;
 use Michaelr0\Buildamic\Fields\Field;
 use Michaelr0\Buildamic\Fields\Fields;
 use Michaelr0\Buildamic\Fieldtypes\Buildamic;
-use Michaelr0\Buildamic\Traits\Hydration;
-use Statamic\Facades\Antlers;
 use Statamic\Fields\Value;
+use Statamic\View\View;
 
 class BuildamicRenderer
 {
@@ -47,17 +46,17 @@ class BuildamicRenderer
 
     public function renderSection(Value $section)
     {
-        return view("{$this->viewPrefix}.layouts.section", ['buildamic' => $this, 'section' => $section]);
+        return View::make("{$this->viewPrefix}.layouts.section", ['buildamic' => $this, 'section' => $section]);
     }
 
     public function renderRow(Value $row)
     {
-        return view("{$this->viewPrefix}.layouts.row", ['buildamic' => $this, 'row' => $row]);
+        return View::make("{$this->viewPrefix}.layouts.row", ['buildamic' => $this, 'row' => $row]);
     }
 
     public function renderColumn(Value $column)
     {
-        return view("{$this->viewPrefix}.layouts.column", ['buildamic' => $this, 'column' => $column]);
+        return View::make("{$this->viewPrefix}.layouts.column", ['buildamic' => $this, 'column' => $column]);
     }
 
     public function renderField($field)
@@ -74,19 +73,15 @@ class BuildamicRenderer
     public function renderSingleField(Field $field)
     {
         // type: markdown, handle:hero-blurb, file: markdown-hero-blurb
-        if (view()->exists("{$this->viewPrefix}.fields.{$field->type()}-{$field->handle('handle')}")) {
-            $view = "{$this->viewPrefix}.fields.{$field->type()}-{$field->handle('handle')}";
-        }
-        // type: markdown, file: markdown
-        elseif (view()->exists("{$this->viewPrefix}.fields.{$field->type()}")) {
-            $view = "{$this->viewPrefix}.fields.{$field->type()}";
-        }
-        // catch all, file: default-field
-        else {
-            $view = "{$this->viewPrefix}.default-field";
-        }
+        $viewFromTypeAndHandle = "{$this->viewPrefix}.fields.{$field->type()}-{$field->handle('handle')}";
 
-        return view($view, ['buildamic' => $this, 'field' => $field]);
+        // type: markdown, file: markdown
+        $viewFromType = "{$this->viewPrefix}.fields.{$field->type()}";
+
+        // catch all, file: default-field
+        $fallbackView = "{$this->viewPrefix}.default-field";
+
+        return View::first([$viewFromTypeAndHandle, $viewFromType, $fallbackView], ['buildamic' => $this, 'field' => $field]);
     }
 
     public function renderFieldset(Fields $fieldset)
@@ -100,7 +95,7 @@ class BuildamicRenderer
 
         // handle:blurb, file: blurb
         if (view()->exists("{$this->viewPrefix}.fieldsets.{$handle}")) {
-            return view("{$this->viewPrefix}.fieldsets.{$handle}", ['buildamic' => $this, 'field' => $fieldset, 'fields' => $fieldset->all()]);
+            return View::make("{$this->viewPrefix}.fieldsets.{$handle}", ['buildamic' => $this, 'field' => $fieldset, 'fields' => $fieldset->all()]);
         }
 
         // catch all, render individual fields.
@@ -122,7 +117,7 @@ class BuildamicRenderer
 
         // handle:blurb, file: blurb
         if (view()->exists("{$this->viewPrefix}.sets.{$set->handle()}")) {
-            return view("{$this->viewPrefix}.sets.{$set->handle()}", ['buildamic' => $this, 'field' => $set, 'fields' => collect($fields)]);
+            return View::make("{$this->viewPrefix}.sets.{$set->handle()}", ['buildamic' => $this, 'field' => $set, 'fields' => collect($fields)]);
         }
 
         // catch all, render individual fields.
