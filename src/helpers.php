de0bd3130bd69fcaa585855ded2ed3c6b3a922d6
Michael Rook <michael@rook.net.au>
2021-07-14 17:00:14 +1000

    Add glide_url helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,20 @@
 <?php
+
+use Statamic\Assets\Asset;
+
+if (! function_exists('glide_url')) {
+    function glide_url(string | Asset $asset, array $params = [])
+    {
+        if ($asset instanceof Asset) {
+            $asset = $asset->id();
+        }
+
+        unset($params['src']);
+        unset($params['id']);
+        unset($params['path']);
+
+        $params['src'] = $asset;
+
+        return tag('glide', $params);
+    }
+}
