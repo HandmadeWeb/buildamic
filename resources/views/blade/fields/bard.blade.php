d1b24048ded26b1aa99a0f71771020c7d9c1a885
Michael Rook <michael@rook.net.au>
2021-07-19 12:13:59 +1000

    Remove multiple value value calls.

--- resources/views/blade/fields/bard.blade.php
+++ resources/views/blade/fields/bard.blade.php
@@ -1,13 +1,16 @@
 @extends('buildamic::blade.layouts.field')
 
+@php 
+    $bardFields = $field->value()->value();
+@endphp
+
 @section('field_content')
-    {{-- @dd($field->get('sets')) --}}
-    @if(is_array($field->value()->value()))
-        @foreach($field->value()->value() as $bardItem)
+    @if(is_array($bardFields))
+        @foreach($bardFields as $bardItem)
             @if(isset($bardItem['text']))
                 {!! $bardItem['text'] !!}
             @else
-                @dd($bardItem)
+                {{-- @dd($bardItem) --}}
             @endif
         @endforeach
     @else
