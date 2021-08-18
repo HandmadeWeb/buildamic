0e61c94d098fc3528fcb5c5fe522e2c794b18487
Michael Rook <michael@rook.net.au>
2021-08-18 12:53:10 +1000

    Disable HTMLMin

--- resources/views/blade/default-field.blade.php
+++ resources/views/blade/default-field.blade.php
@@ -1,6 +1,10 @@
 @extends('buildamic::blade.layouts.field')
 
 @section('field_content')
+    <!-- Disable minification -->
+    <!-- skip.minification -->
+    <!-- /Disable minification -->
+
     <!-- Field could not be rendered, View not found -->
     <!-- Type: {{ $field->type() }} -->
     <!-- Handle: {{ $field->handle() }} -->
