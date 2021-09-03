86a765b5ab7ef9edf905385b2284e1f3beef8eb9
Michael Rook <michael@rook.net.au>
2021-09-03 22:40:40 +1000

    Add additional debug info from #16 to missing fields, thanks to @jelleroorda

--- resources/views/blade/default-field.blade.php
+++ resources/views/blade/default-field.blade.php
@@ -11,4 +11,14 @@
     <!-- Type: {{ $field->type() }} -->
     <!-- Handle: {{ $field->handle() }} -->
 
+    @if(config('app.debug'))
+        <div class="bg-gray-100 border border-gray-200 text-xs text-left p-2">
+            <p>This <strong><span class="inline-block border-b-2 border-gray-200 border-dotted" title="Field type">{{ $field->type() }}</span>::<span title="Field handle">{{ $field->handle() }}</span></strong> field could not be rendered, corresponding views not found.
+            Create one of the following options in your `resources/views/vendor/buildamic/` folder:</p>
+            <ul class="list-disc list-inside">
+                <li>blade/{{ $field->type() }}-{{ $field->handle() }}.blade.php</li>
+                <li>blade/{{ $field->type() }}.blade.php</li>
+            </ul>
+        </div>
+    @endif
 @overwrite
