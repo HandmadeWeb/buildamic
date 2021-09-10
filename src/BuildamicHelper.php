58b72c120fda428744e9d11c886b846471738e78
Michael Rook <michael@rook.net.au>
2021-09-10 11:01:43 +1000

    Add alias helper

---
+++ src/BuildamicHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HandmadeWeb\Buildamic;
+
+use Statamic\Contracts\Entries\Entry as EntryContract;
+use Statamic\Entries\Entry;
+use Statamic\Facades\Entry as EntryFacade;
+
+class BuildamicHelper
+{
+}
