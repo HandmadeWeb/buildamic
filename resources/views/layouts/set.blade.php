6d147d1392fadeb102116996fecac5af6d634ea5
Yohan <john@inertiainmotion.com.au>
2022-02-15 11:16:13 +1100

    Fix $set

--- resources/views/layouts/set.blade.php
+++ resources/views/layouts/set.blade.php
@@ -1,5 +1,5 @@
 @php
-    $moduleLink = $field->buildamicSetting('moduleLink') ?? null;
+    $moduleLink = $set->buildamicSetting('moduleLink') ?? null;
 @endphp
 
 <div @isset($moduleLink) onclick="location.href='{{ $moduleLink }}';" @endisset {!! BuildamicHelper()->HtmlId($set->buildamicSetting('attributes.id')) !!} class="buildamic-set @isset($moduleLink) cursor-pointer @endisset {{ $set->computedAttribute('class') }}">
