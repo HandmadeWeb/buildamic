32a2c5688d0e1effb6e3c37279b8100a71c8907b
Yohan <john@inertiainmotion.com.au>
2022-02-15 11:06:04 +1100

    Add moduleLink to frontend fields/sets

--- resources/views/layouts/field.blade.php
+++ resources/views/layouts/field.blade.php
@@ -1,3 +1,7 @@
-<div {!! BuildamicHelper()->HtmlId($field->buildamicSetting('attributes.id')) !!} {{ $field->computedAttribute()['dataAtts'] }} class="buildamic-field {{ $field->computedAttribute('class') }}">
+@php
+    $moduleLink = $field->buildamicSetting('moduleLink') ?? null;
+@endphp
+
+<div @isset($moduleLink) onclick="location.href='{{ $moduleLink }}';" @endisset {!! BuildamicHelper()->HtmlId($field->buildamicSetting('attributes.id')) !!} {{ $field->computedAttribute()['dataAtts'] }} class="buildamic-field @isset($moduleLink) cursor-pointer @endisset {{ $field->computedAttribute('class') }}">
     @yield('field_content')
 </div>
--- resources/views/layouts/set.blade.php
+++ resources/views/layouts/set.blade.php
@@ -1,3 +1,7 @@
-<div {!! BuildamicHelper()->HtmlId($set->buildamicSetting('attributes.id')) !!} class="buildamic-set {{ $set->computedAttribute('class') }}">
+@php
+    $moduleLink = $field->buildamicSetting('moduleLink') ?? null;
+@endphp
+
+<div @isset($moduleLink) onclick="location.href='{{ $moduleLink }}';" @endisset {!! BuildamicHelper()->HtmlId($set->buildamicSetting('attributes.id')) !!} class="buildamic-set @isset($moduleLink) cursor-pointer @endisset {{ $set->computedAttribute('class') }}">
     @yield('set_content')
 </div>
