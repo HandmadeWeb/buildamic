1bbf317819acac035fe8786712a10d8c88522a32
Michael Rook <michael@rook.net.au>
2022-03-02 10:01:31 +1100

    Update namespace

--- resources/views/default-field.blade.php
+++ resources/views/default-field.blade.php
@@ -6,7 +6,7 @@
         <!-- skip.minification -->
         <!-- /Disable minification -->
     @endif
-    
+
     <!-- Field could not be rendered, View not found -->
     <!-- Type: {{ $field->type() }} -->
     <!-- Handle: {{ $field->handle() }} -->
@@ -16,8 +16,8 @@
             <p>This <strong><span class="inline-block border-b-2 border-gray-200 border-dotted" title="Field type">{{ $field->type() }}</span>::<span title="Field handle">{{ $field->handle() }}</span></strong> field could not be rendered, corresponding views not found.
             Create one of the following options in your `resources/views/vendor/buildamic/` folder:</p>
             <ul class="list-disc list-inside">
-                <li>blade/{{ $field->type() }}-{{ $field->handle() }}.blade.php</li>
-                <li>blade/{{ $field->type() }}.blade.php</li>
+                <li>{{ $field->type() }}-{{ $field->handle() }}.blade.php</li>
+                <li>{{ $field->type() }}.blade.php</li>
             </ul>
         </div>
     @endif
