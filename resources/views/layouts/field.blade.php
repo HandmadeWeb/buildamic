1a400aa11ab5caab72dac67d989134156353fb3a
Michael Rook <michael@rook.net.au>
2022-03-02 10:02:10 +1100

    Adjust display logic check

--- resources/views/layouts/field.blade.php
+++ resources/views/layouts/field.blade.php
@@ -1,7 +1,3 @@
-@php
-    $moduleLink = $field->buildamicSetting('moduleLink') ?? null;
-@endphp
-
-<div @isset($moduleLink) onclick="location.href='{{ $moduleLink }}';" @endisset {!! BuildamicHelper()->HtmlId($field->buildamicSetting('attributes.id')) !!} {{ $field->computedAttribute()['dataAtts'] }} class="buildamic-field @isset($moduleLink) cursor-pointer @endisset {{ $field->computedAttribute('class') }}">
+<div @if($moduleLink = $field->buildamicSetting('moduleLink')) onclick="location.href='{{ $moduleLink }}';" @endif {!! BuildamicHelper()->HtmlId($field->buildamicSetting('attributes.id')) !!} {{ $field->computedAttribute('dataAtts') }} class="buildamic-field @if($moduleLink) cursor-pointer @endif {{ $field->computedAttribute('class') }}">
     @yield('field_content')
 </div>
