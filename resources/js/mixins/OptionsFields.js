5ba6a881cbed22fafae00dbe0c43193e0d749f9d
Michael Rook <michael@rook.net.au>
2022-03-02 12:00:18 +1100

    Use local tailwind.config.js ?

--- resources/js/mixins/OptionsFields.js
+++ resources/js/mixins/OptionsFields.js
@@ -1,5 +1,6 @@
 import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../../../../../tailwind.config.js";
+// import tailwindConfig from "../../../../../../tailwind.config.js";
+import tailwindConfig from "../../../tailwind.config.js";
 
 const fullConfig = resolveConfig(tailwindConfig);
 
