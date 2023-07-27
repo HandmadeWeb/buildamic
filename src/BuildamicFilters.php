a188da181a2af8570235c71036185733bbe76ca7
Yohan <john@handmadeweb.com.au>
2023-07-27 12:26:01 +1000

    Fix missing modify method

--- src/BuildamicFilters.php
+++ src/BuildamicFilters.php
@@ -5,239 +5,241 @@ namespace HandmadeWeb\Buildamic;
 use HandmadeWeb\Buildamic\Fields\Field;
 use HandmadeWeb\Buildamic\Fields\Fields;
 
+use Statamic\Modifiers\Modify;
+
 class BuildamicFilters
 {
-    /**
-     * Define Filter/Method's to register.
-     *
-     * The $filters array expects the following format:
-     * 'filter_name' => 'method_name',
-     *
-     * Where filter_name will be used by Filter::add() and Filter::run() function calls.
-     * And method_name is the public static function that should be called for that filter.
-     *
-     * $data will always be passed to these filters.
-     *
-     * @var array
-     */
-    protected static $filters = [
-        'buildamic_filter_everything' => 'filter_everything',
-        // 'buildamic_filter_section' => 'filter_section',
-        'buildamic_filter_row' => 'filter_row',
-        'buildamic_filter_column' => 'filter_column',
-        'buildamic_filter_field' => 'filter_field',
-        // 'buildamic_filter_field:markdown-blurb' => 'filter_field_markdown_handle_blurb',
-        // 'buildamic_filter_set' => 'filter_set',
-        // 'buildamic_filter_set:blurb' => 'filter_set_blurb',
-    ];
+  /**
+   * Define Filter/Method's to register.
+   *
+   * The $filters array expects the following format:
+   * 'filter_name' => 'method_name',
+   *
+   * Where filter_name will be used by Filter::add() and Filter::run() function calls.
+   * And method_name is the public static function that should be called for that filter.
+   *
+   * $data will always be passed to these filters.
+   *
+   * @var array
+   */
+  protected static $filters = [
+    'buildamic_filter_everything' => 'filter_everything',
+    // 'buildamic_filter_section' => 'filter_section',
+    'buildamic_filter_row' => 'filter_row',
+    'buildamic_filter_column' => 'filter_column',
+    'buildamic_filter_field' => 'filter_field',
+    // 'buildamic_filter_field:markdown-blurb' => 'filter_field_markdown_handle_blurb',
+    // 'buildamic_filter_set' => 'filter_set',
+    // 'buildamic_filter_set:blurb' => 'filter_set_blurb',
+  ];
 
-    public static function boot()
-    {
-        foreach (static::$filters as $filter_name => $method_name) {
-            if (method_exists(static::class, $method_name)) {
-                Filter::add($filter_name, [static::class, $method_name], 10, 1);
-            }
+  public static function boot()
+  {
+    foreach (static::$filters as $filter_name => $method_name) {
+      if (method_exists(static::class, $method_name)) {
+        Filter::add($filter_name, [static::class, $method_name], 10, 1);
+      }
+    }
+  }
+
+  // public static function example_filter(Field $data): Field
+  // {
+  //     // Can use computedAttributes.
+  //
+  //     // set/replace computed attributes with the provided.
+  //     $data->field()->setcomputedAttributes(['foo' => 'bar']);
+  //     $data->field()->computedAttributes();
+  //     // [
+  //     //     "foo" => "bar"
+  //     // ]
+  //     $data->field()->computedAttribute('foo');
+  //     // Bar
+
+  //     // add/merge new data into the computed attribuites.
+  //     $data->field()->mergeComputedAttributes(['bar' => 'foo']);
+  //     $data->field()->computedAttributes();
+  //     // [
+  //     //     "foo" => "bar",
+  //     //     "bar" => "foo"
+  //     // ]
+  //     $data->field()->computedAttribute('bar');
+  //     // Foo
+
+  //     // set/replace computed attributes with the provided.
+  //     $data->field()->setcomputedAttributes(['bar' => 'foo']);
+  //     $data->field()->computedAttributes();
+  //     // [
+  //     //     "bar" => "foo"
+  //     // ]
+  //     $data->field()->computedAttribute('bar');
+  //     // Foo
+  //     $data->field()->computedAttribute('foo');
+  //     // null or $fallback (second property on computedAttribute method)
+
+  //     // Must be returned!
+  //     return $data;
+  // }
+
+  public static function filter_everything(Field|Fields $field)
+  {
+    $classList = collect(explode(' ', $field->buildamicSetting('attributes.class')))->filter()->toArray();
+    $dataAtts = static::get_data_attributes(collect($field->buildamicSetting('attributes.dataAtts'))->filter()->toArray()) ?? [];
+
+    if (is_array($field->buildamicSetting('inline'))) {
+      // Remove anything we don't want generated with the loop (e.g background is handled separately)
+      $inlineClassList = collect($field->buildamicSetting('inline'))->filter(function ($value, $key) {
+        return 'background' !== $key;
+      })->filter()->toArray();
+
+      if (!empty($inlineClassList)) {
+        foreach ($inlineClassList as $item) {
+          $classList[] = static::get_tw_classes_from_inline_atts($item);
         }
+      }
+
+      if (!empty($field->buildamicSetting('inline.background'))) {
+        // [] loop the background options
+      }
     }
 
-    // public static function example_filter(Field $data): Field
-    // {
-    //     // Can use computedAttributes.
-    //
-    //     // set/replace computed attributes with the provided.
-    //     $data->field()->setcomputedAttributes(['foo' => 'bar']);
-    //     $data->field()->computedAttributes();
-    //     // [
-    //     //     "foo" => "bar"
-    //     // ]
-    //     $data->field()->computedAttribute('foo');
-    //     // Bar
+    $background_image = $field->buildamicSetting('inline')['background']['image']['value'][0] ?? null;
+    $background_video = $field->buildamicSetting('inline')['background']['video'] ?? null;
 
-    //     // add/merge new data into the computed attribuites.
-    //     $data->field()->mergeComputedAttributes(['bar' => 'foo']);
-    //     $data->field()->computedAttributes();
-    //     // [
-    //     //     "foo" => "bar",
-    //     //     "bar" => "foo"
-    //     // ]
-    //     $data->field()->computedAttribute('bar');
-    //     // Foo
+    if (isset($background_image)) {
+      $field->mergeComputedAttributes(['background_image' => glide_url($background_image)]);
+    }
+    if (isset($background_video)) {
+      $field->mergeComputedAttributes(['background_video' => $background_video]);
+    }
+    $field->mergeComputedAttributes(['class' => implode(' ', $classList)]);
+    $field->mergeComputedAttributes(['dataAtts' => implode(' ', $dataAtts)]);
 
-    //     // set/replace computed attributes with the provided.
-    //     $data->field()->setcomputedAttributes(['bar' => 'foo']);
-    //     $data->field()->computedAttributes();
-    //     // [
-    //     //     "bar" => "foo"
-    //     // ]
-    //     $data->field()->computedAttribute('bar');
-    //     // Foo
-    //     $data->field()->computedAttribute('foo');
-    //     // null or $fallback (second property on computedAttribute method)
+    return $field;
+  }
 
-    //     // Must be returned!
-    //     return $data;
-    // }
+  // public static function filter_section(Field $section): Field
+  // {
+  //     return $section;
+  // }
 
-    public static function filter_everything(Field|Fields $field)
-    {
-        $classList = collect(explode(' ', $field->buildamicSetting('attributes.class')))->filter()->toArray();
-        $dataAtts = static::get_data_attributes(collect($field->buildamicSetting('attributes.dataAtts'))->filter()->toArray()) ?? [];
+  public static function filter_row(Field $row): Field
+  {
+    $colGaps = static::get_col_gap(collect($row->buildamicSetting('attributes.col_gap'))->filter()->toArray()) ?? [];
+    $columnCountTotal = static::column_count_total($row->buildamicSetting('attributes.column_count_total') ?? 12);
+    $row->mergeComputedAttributes(['col_gap' => implode(' ', $colGaps)]);
+    $row->mergeComputedAttributes(['column_count_total' => $columnCountTotal]);
 
-        if (is_array($field->buildamicSetting('inline'))) {
-            // Remove anything we don't want generated with the loop (e.g background is handled separately)
-            $inlineClassList = collect($field->buildamicSetting('inline'))->filter(function ($value, $key) {
-                return 'background' !== $key;
-            })->filter()->toArray();
+    return $row;
+  }
 
-            if (!empty($inlineClassList)) {
-                foreach ($inlineClassList as $item) {
-                    $classList[] = static::get_tw_classes_from_inline_atts($item);
-                }
-            }
-
-            if (!empty($field->buildamicSetting('inline.background'))) {
-                // [] loop the background options
-            }
+  public static function filter_column(Field $column): Field
+  {
+    if (!empty($column->buildamicSetting('columnSizes'))) {
+      $columnSizes = collect($column->buildamicSetting('columnSizes'))->map(function ($value, $key) {
+        if (!empty($value)) {
+          if ('xs' == $key) {
+            return "col-{$value} ";
+          } else {
+            return "{$key}:col-{$value} ";
+          }
         }
+      })->filter()->implode(' ');
 
-        $background_image = $field->buildamicSetting('inline')['background']['image']['value'][0] ?? null;
-        $background_video = $field->buildamicSetting('inline')['background']['video'] ?? null;
-
-        if (isset($background_image)) {
-            $field->mergeComputedAttributes(['background_image' => glide_url($background_image)]);
-        }
-        if (isset($background_video)) {
-            $field->mergeComputedAttributes(['background_video' => $background_video]);
-        }
-        $field->mergeComputedAttributes(['class' => implode(' ', $classList)]);
-        $field->mergeComputedAttributes(['dataAtts' => implode(' ', $dataAtts)]);
-
-        return $field;
+      if (!empty($columnSizes)) {
+        $column->mergeComputedAttributes(['class' => $column->computedAttribute('class') . " {$columnSizes}"]);
+      }
     }
 
-    // public static function filter_section(Field $section): Field
-    // {
-    //     return $section;
-    // }
+    return $column;
+  }
 
-    public static function filter_row(Field $row): Field
-    {
-        $colGaps = static::get_col_gap(collect($row->buildamicSetting('attributes.col_gap'))->filter()->toArray()) ?? [];
-        $columnCountTotal = static::column_count_total($row->buildamicSetting('attributes.column_count_total') ?? 12);
-        $row->mergeComputedAttributes(['col_gap' => implode(' ', $colGaps)]);
-        $row->mergeComputedAttributes(['column_count_total' => $columnCountTotal]);
+  public static function filter_field(Field $field): Field
+  {
+    $field->mergeComputedAttributes(['class' => Modify::value($field->type())->ensureLeft('buildamic-')->ensureRight('-field') . ' ' . $field->computedAttribute('class')]);
 
-        return $row;
+    return $field;
+  }
+
+  // public static function filter_field_markdown_handle_blurb(Field $field): Field
+  // {
+  //     return $field;
+  // }
+
+  // public static function filter_set_blurb(Value $data): Value
+  // {
+  //     return $data;
+  // }
+
+  /**
+   * Takes all the inline attributes, font-size, margins, everything, then concatinates them into a big
+   * string of tailwind classes.
+   */
+  protected static function get_tw_classes_from_inline_atts(array|string $classes): string
+  {
+    $generatedClasses = [];
+
+    if (is_array($classes)) {
+      foreach ($classes as $child) {
+        if (!is_array($child) && !empty($child)) {
+          $generatedClasses[] = $child;
+        } elseif (is_array($child)) {
+          $generatedClasses[] = static::get_tw_classes_from_inline_atts($child);
+        }
+      }
+    } else {
+      $generatedClasses[] = $classes;
     }
 
-    public static function filter_column(Field $column): Field
-    {
-        if (!empty($column->buildamicSetting('columnSizes'))) {
-            $columnSizes = collect($column->buildamicSetting('columnSizes'))->map(function ($value, $key) {
-                if (!empty($value)) {
-                    if ('xs' == $key) {
-                        return "col-{$value} ";
-                    } else {
-                        return "{$key}:col-{$value} ";
-                    }
-                }
-            })->filter()->implode(' ');
+    return trim(implode(' ', $generatedClasses), ' ');
+  }
 
-            if (!empty($columnSizes)) {
-                $column->mergeComputedAttributes(['class' => $column->computedAttribute('class')." {$columnSizes}"]);
-            }
-        }
+  protected static function get_data_attributes(array $dataAtts = []): array
+  {
+    $generatedAtts = [];
 
-        return $column;
+    if (is_array($dataAtts) && !empty($dataAtts)) {
+      foreach ($dataAtts as $att) {
+        $generatedAtts[] = "data-{$att['key']}={$att['value']}";
+      }
     }
 
-    public static function filter_field(Field $field): Field
-    {
-        $field->mergeComputedAttributes(['class' => modify($field->type())->ensureLeft('buildamic-')->ensureRight('-field').' '.$field->computedAttribute('class')]);
+    return $generatedAtts;
+  }
 
-        return $field;
+  protected static function get_col_gap(array $colgaps = []): array
+  {
+    $generatedClasses = [];
+
+    if (!is_array($colgaps) || empty($colgaps)) {
+      return $colgaps;
     }
 
-    // public static function filter_field_markdown_handle_blurb(Field $field): Field
-    // {
-    //     return $field;
-    // }
-
-    // public static function filter_set_blurb(Value $data): Value
-    // {
-    //     return $data;
-    // }
-
-    /**
-     * Takes all the inline attributes, font-size, margins, everything, then concatinates them into a big
-     * string of tailwind classes.
-     */
-    protected static function get_tw_classes_from_inline_atts(array|string $classes): string
-    {
-        $generatedClasses = [];
-
-        if (is_array($classes)) {
-            foreach ($classes as $child) {
-                if (!is_array($child) && !empty($child)) {
-                    $generatedClasses[] = $child;
-                } elseif (is_array($child)) {
-                    $generatedClasses[] = static::get_tw_classes_from_inline_atts($child);
-                }
-            }
-        } else {
-            $generatedClasses[] = $classes;
-        }
-
-        return trim(implode(' ', $generatedClasses), ' ');
+    foreach ($colgaps as $breakpoint => $colgap) {
+      // dd($breakpoint);
+      if ('xs' == $breakpoint) {
+        $generatedClasses[] = "gap-{$colgap}";
+      } else {
+        $generatedClasses[] = "{$breakpoint}:gap-{$colgap}";
+      }
     }
 
-    protected static function get_data_attributes(array $dataAtts = []): array
-    {
-        $generatedAtts = [];
+    return $generatedClasses;
+  }
 
-        if (is_array($dataAtts) && !empty($dataAtts)) {
-            foreach ($dataAtts as $att) {
-                $generatedAtts[] = "data-{$att['key']}={$att['value']}";
-            }
-        }
-
-        return $generatedAtts;
+  protected static function column_count_total(int $columnCountTotal = 12): string
+  {
+    if (empty($columnCountTotal)) {
+      return '';
+    }
+    if (0 != (int) $columnCountTotal % 12) {
+      return "--b-columns: {$columnCountTotal};";
     }
 
-    protected static function get_col_gap(array $colgaps = []): array
-    {
-        $generatedClasses = [];
+    return '';
+  }
 
-        if (!is_array($colgaps) || empty($colgaps)) {
-            return $colgaps;
-        }
-
-        foreach ($colgaps as $breakpoint => $colgap) {
-            // dd($breakpoint);
-            if ('xs' == $breakpoint) {
-                $generatedClasses[] = "gap-{$colgap}";
-            } else {
-                $generatedClasses[] = "{$breakpoint}:gap-{$colgap}";
-            }
-        }
-
-        return $generatedClasses;
-    }
-
-    protected static function column_count_total(int $columnCountTotal = 12): string
-    {
-        if (empty($columnCountTotal)) {
-            return '';
-        }
-        if (0 != (int) $columnCountTotal % 12) {
-            return "--b-columns: {$columnCountTotal};";
-        }
-
-        return '';
-    }
-
-    // protected static function get_inline_styles(array | string $attribute = ''): string
-    // {
-    //     // [] get the background options and create an inline style attribute string
-    // }
+  // protected static function get_inline_styles(array | string $attribute = ''): string
+  // {
+  //     // [] get the background options and create an inline style attribute string
+  // }
 }
